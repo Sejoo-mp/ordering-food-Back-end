bff8b46413632a8f2fc672a1876e22285c946f1f
Sejoo-mp <fs2013644@gmail.com>
2026-06-24 10:24:37 +0430

    add create order endpoint

---
+++ routers/orders.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, HTTPException
+from models.order import Order
+from models.menu import MenuItem
+from schemas.orders import CreateOrderSchema
+
+router = APIRouter(prefix="/orders", tags=["Orders"])
+
+@router.post("/")
+async def create_order(data: CreateOrderSchema):
+    total_price = 0
+    items = []
+    for item in data.items:
+        menu_item = await MenuItem.get(item.menu_item_id)
+        if not menu_item:
+            raise HTTPException(status_code=404, detail="Menu item not found")
+        if not menu_item.is_available:
+            raise HTTPException(status_code=400, detail="Item unavailable")
+        total_price += menu_item.price * item.quantity
+        items.append({
+            "menu_item_id": str(menu_item.id),
+            "quantity": item.quantity,
+            "price": menu_item.price
+        })
+    order = Order(user_id="user_id", items=items, total_price=total_price)
+    await order.insert()
+    return order
