baaa5cb63ce242ce4cfb5e1f58423de7da13d11d
Sejoo-mp <fs2013644@gmail.com>
2026-06-24 10:29:52 +0430

    register all routers in main app

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from database import init_db
+from routers.auth import router as auth_router
+from routers.menu import router as menu_router
+from routers.orders import router as orders_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -8,6 +11,10 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(auth_router)
+app.include_router(menu_router)
+app.include_router(orders_router)
 
 @app.get("/")
 async def root():
